bce6af30a89dcc2e9c4bcd06ad363eecfbdfbdcb
Shubham <Shubham@LENOVO.Dlink>
2024-05-30 12:18:34 +0200

    My first Commit

--- Practice/src/com/Practice/questions/qustions.java
+++ Practice/src/com/Practice/questions/qustions.java
@@ -1,6 +1,7 @@
 package com.Practice.questions;
 public class qustions {
-
+	
+	//Printing the Output message
 	public static void main(String[] args) {
 		System.out.println("My first file in git");
 
